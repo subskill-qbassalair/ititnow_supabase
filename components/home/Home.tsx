8bdb081f6a6cebb95bed055da87cfdd2e5792c23
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-05-14 14:31:29 +0200

    prepare mapbox

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import {View,Text} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 import { Session } from '@supabase/supabase-js'
+import CustomMap from "./CustomMap";
 
 
 
 function Home({ session }: { session: Session }) {
     return (
-        <View>
+        <View style={styles.container} >
             <Text>Home</Text>
+            <CustomMap/>
+            <Text>{session.user?.email}</Text>
         </View>
     );
 }
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+  }
+})
